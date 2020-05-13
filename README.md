acca8c2e01949a82fbfa394d7619f6f16d62faf4
Marko Knoebl <marko.kn@gmail.com>
2020-05-14 00:14:35 +0200

    support both gfm and commonmark

--- README.md
+++ README.md
@@ -2,5 +2,10 @@ Escape Markdown control characters.
 
 ```javascript
 var escape = require('markdown-escape')
-require('assert')(escape("#1! We're #1!") === "\\#1! We're \\#1!")
+var assert = require('assert')
+
+assert(escape("#1! We're #1!") === "\\#1\\! We\\'re \\#1\\!")
+assert(escape("#1! We're #1!", {flavor: "gfm"}) === "\\#1\\! We're \\#1\\!")
+assert(escape("https://google.com", {flavor: "commonmark"}) === "https\\:\\/\\/google\\.com")
+assert(escape("https://google.com", {flavor: "gfm"}) === "https://google\\.com")
 ```
--- index.js
+++ index.js
@@ -1,18 +1,33 @@
-var replacements = [
-  [/\*/g, '\\*'],
-  [/#/g, '\\#'],
-  [/\//g, '\\/'],
-  [/\(/g, '\\('],
-  [/\)/g, '\\)'],
-  [/\[/g, '\\['],
-  [/\]/g, '\\]'],
-  [/</g, '&lt;'],
-  [/>/g, '&gt;'],
-  [/_/g, '\\_']
-]
+const markdownEscapes = require("markdown-escapes");
 
-module.exports = function (string) {
-  return replacements.reduce(function (string, replacement) {
-    return string.replace(replacement[0], replacement[1])
-  }, string)
-}
+const htmlEntities = { "<": "&lt;", "&": "&amp;" };
+
+const escape = (input, options={}) => {
+  const flavor = options.flavor || "commonmark";
+  let backslashEscapableCharacters;
+  let entityEscapableCharacters;
+  if (flavor === "gfm") {
+    backslashEscapableCharacters = markdownEscapes.gfm;
+    entityEscapableCharacters = ["<", "&"];
+  } else {
+    backslashEscapableCharacters = markdownEscapes.commonmark;
+    entityEscapableCharacters = [];
+  }
+  const escapableCharacters = backslashEscapableCharacters.concat(
+    entityEscapableCharacters
+  );
+  const escapableCharactersWithBackslash = escapableCharacters.map(
+    character => "\\" + character
+  );
+  const regexpString = "(" + escapableCharactersWithBackslash.join("|") + ")";
+  const regexp = new RegExp(regexpString, "g");
+  return input.replace(regexp, match => {
+    if (backslashEscapableCharacters.includes(match)) {
+      return "\\" + match;
+    } else {
+      return htmlEntities[match];
+    }
+  });
+};
+
+module.exports = escape;
